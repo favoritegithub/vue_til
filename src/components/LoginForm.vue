39ab939265cffbc0778aa2c96cca4d99ddd65fd8
favoritegithub <favoritemail.sooyoung@gmail.com>
2020-05-14 09:41:41 +0900

    로그인 폼 컴포넌트 생성

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,19 @@
+<template>
+	<form>
+		<div>
+			<label for="username"></label>
+			<input id="username" type="" />
+		</div>
+		<div>
+			<label for="password"></label>
+			<input id="password" type="" />
+		</div>
+		<button type="submit">로그인</button>
+	</form>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
